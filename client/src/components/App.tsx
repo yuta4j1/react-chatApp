28e2ba201b090a992cf01cbfda5bcac677b3886b
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-02-12 00:26:13 +0900

    Update layout

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,24 @@
-import * as React from 'react'
-import Header from './Header'
+import * as React from 'react';
+import Header from './Header';
+import InputMessage from './InputMessage';
+import * as styles from '../css/app.css';
 
 const App = (): JSX.Element => {
-    const logoTitle = 'chat room';
+  const logoTitle = 'chat room';
+  const initMessage = {
+    value: ''
+  };
   return (
-      <Header value={logoTitle} />
-  )
-}
+    <div>
+      <div className={styles.header}>
+        <Header value={logoTitle} />
+      </div>
+      <div className={styles.body} />
+      <div className={styles.footer}>
+        <InputMessage props={initMessage} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
