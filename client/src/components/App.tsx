5fb33be75a778ded2d4e17c463138fb32a3ff1f9
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-02-13 23:39:15 +0900

    Add state for App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,22 +3,43 @@ import Header from './Header';
 import InputMessage from './InputMessage';
 import * as styles from '../css/app.css';
 
-const App = (): JSX.Element => {
-  const logoTitle = 'chat room';
-  const initMessage = {
-    value: ''
-  };
-  return (
-    <div>
-      <div className={styles.header}>
-        <Header value={logoTitle} />
-      </div>
-      <div className={styles.body} />
-      <div className={styles.footer}>
-        <InputMessage props={initMessage} />
-      </div>
-    </div>
-  );
+type State = {
+  messages: Message[];
 };
+
+class App extends React.Component<{}, State> {
+  constructor({}) {
+    super({});
+    this.state = {
+      messages: []
+    };
+  }
+
+  updateChannel(msg: Message) {
+    this.state.messages.push(msg);
+    const arr: Message[] = this.state.messages;
+    this.setState({
+      messages: arr
+    });
+  }
+
+  render() {
+    const logoTitle = 'chat room';
+    return (
+      <div>
+        <div className={styles.header}>
+          <Header value={logoTitle} />
+        </div>
+        <div className={styles.body} />
+        <div className={styles.footer}>
+          <InputMessage
+            value={''}
+            reflectChannel={(msg: Message) => this.updateChannel(msg)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
