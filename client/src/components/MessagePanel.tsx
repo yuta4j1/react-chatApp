d0f0dd7558c7ad75d2ec5cefe7d772c140e5cc98
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-02-15 00:02:49 +0900

    Add MessagePanel component

---
+++ client/src/components/MessagePanel.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as styles from '../css/messagePanel.css';
+
+type PanelProps = {
+  message: Message;
+};
+
+class MessagePanel extends React.Component<PanelProps, {}> {
+  render() {
+    return (
+      <div className={styles.panelContainer}>
+        <div className={styles.panelItem}>
+          <div>ICON</div>
+        </div>
+        <div className={styles.panelItem}>
+          <div>ユーザ名</div>
+          <div>{this.props.message}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MessagePanel;
