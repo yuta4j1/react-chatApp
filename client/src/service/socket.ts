d18b80bc58714ae6a0def8da7c096eb95fc7fda0
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-02-14 23:55:43 +0900

    Add function: wrap message with Promise

--- client/src/service/socket.ts
+++ client/src/service/socket.ts
@@ -3,16 +3,17 @@ import * as io from 'socket.io-client';
 const baseUrl = 'http://localhost:1234/';
 const socket: SocketIOClient.Socket = io.connect(baseUrl);
 
-export const sendMessage = (msg: Message) => {
-  let aMessage: Message;
+// サーバから受け取ったmessageをpromiseにくるんで返す
+const messagePromise = (msg: string = 'message') =>
+  new Promise<Message>(resolve => {
+    socket.on(msg, (message: Message) => {
+      resolve(message);
+    });
+  });
+
+export const sendMessage = async (msg: Message): Promise<Message> => {
   if (socket) {
     socket.emit('message', msg);
   }
-  socket.on('message', (msg: Message) => {
-    // TODO １．コールバック内で受け取ったメッセージをどうにか戻り値に渡す
-    // ２． 表示するメッセージをreduxで管理するようにする
-    aMessage = msg;
-    console.log(msg);
-  });
-  // return aMessage;
+  return await messagePromise('message');
 };
